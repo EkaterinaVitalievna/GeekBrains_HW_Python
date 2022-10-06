d09c71377cdb9e6dcfa70e2f483bca8a0bd35160
EkaterinaVitalievna <zinovieva.k@mail.ru>
2022-10-07 00:12:41 +0300

    сумма чисел последовательности

--- HW2/1.py
+++ HW2/1.py
@@ -3,18 +3,9 @@
 # *Пример:*
 # - 6782 -> 23
 
-# real_number = input('Введите вещественное число: ')
-# sum = 0
-# for i in real_number:
-#     if i.isdigit() == True:
-#         sum = sum + int(i)
-# print(sum)
-
-real_number = float(input("Введите вещественное число: "))
-a = len(str(real_number)) - 2
-real_number = int(real_number * 10 ** a)
+real_number = input('Введите вещественное число: ')
 sum = 0
-while real_number > 0:
-    sum = int(sum + real_number % 10)
-    real_number = real_number / 10
+for i in real_number:
+    if i.isdigit() == True:
+        sum = sum + int(i)
 print(sum)
