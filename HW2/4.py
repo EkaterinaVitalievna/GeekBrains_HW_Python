5aac6fa314eec9f6a0af58758e690a02af899da3
EkaterinaVitalievna <zinovieva.k@mail.ru>
2022-10-07 00:48:36 +0300

    заполнение текст.файла позициями элементов и поиск произведения элементов на этих позициях

---
+++ HW2/4.py
@@ -0,0 +1,40 @@
+# Задайте число N, создайте список: [-N, N].
+# Найдите произведение элементов на указанных позициях.
+# Позиции (случайные) хранятся в файле file.txt
+# (создаётся во время выполнения кода и зависит от количества элементов в списке)
+# в одной строке одно число.
+# Пример:
+# Файл:
+# 4
+# 5
+# 2
+# N = 3 => [-3, -2, -1, 0, 1, 2, 3]
+# Результат: 1*2*(-1) = -2
+
+import random
+
+# создание списка
+n = int(input('Введите количество элементов :   '))
+run = range(-n, n, 2)
+numbers = list(run)
+print(numbers)
+
+#Запись позиций в файл
+number = random.randint(2, n)
+f = open('4.1.txt','w')
+for i in range (number):
+    index = random.randint(0,n-1)
+    f.write(str(index))
+    f.write('\n')
+f.close()
+
+# суммирование элементов, соответсвующих позициям в файле.
+multiplicator = 1
+#f = open('text.txt','r')
+#for i in  range (1, number+1):
+with open ("4.1.txt", "r") as f:
+    for i in f :
+        print(i)
+        multiplicator = multiplicator * numbers[int(i)]
+#f.close()
+print(f' произведение чисел под заданными в файле номерами  = {multiplicator}')
